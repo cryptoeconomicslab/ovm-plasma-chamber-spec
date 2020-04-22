90d34fbbf44b722e9dabb21681f710730bf5f6a4
daiki.sekiguchi <daiki.skgc44@gmail.com>
2020-04-22 19:33:50 +0900

    fix introduction path

--- sidebars.js
+++ sidebars.js
@@ -8,8 +8,8 @@ module.exports = {
     ],
   },
   docs: {
-    Introduction: ["Introduction"],
-    "Getting Started": ["getting-started/Try_Plasma_Chamber_In_Local"],
+    Introduction: ["docs/Introduction"],
+    "Getting Started": ["docs/getting-started/Try_Plasma_Chamber_In_Local"],
     Tutorial: [
       "docs/tutorial/cui-wallet/README",
       "docs/tutorial/cui-wallet/Setup",
