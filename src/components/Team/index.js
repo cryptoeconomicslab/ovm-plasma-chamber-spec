307ae920f6cbacd9d31f16b51f5c661bcaaa8cc9
daiki.sekiguchi <daiki.skgc44@gmail.com>
2020-04-24 00:09:31 +0900

    refactoring team

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -3,6 +3,32 @@ import Button from "../Button";
 import Card from "../Card";
 import styles from "./styles.module.scss";
 
+const cards = [
+  {
+    name: "Syuhei Hiya",
+    img: "/img/team/syuhei.png",
+    desc:
+      "Father of 2 lovely boys. Specializes in Programming Language Design. Experienced startup CEO twice and he says CEL is the best team ever.",
+  },
+  {
+    name: "Takamichi Tsutsumi",
+    img: "/img/team/takamichi.png",
+    desc:
+      "Used to be a JavaScript guy at a bunch of startups, while secretly loving low stuff development. Loves watching NBA games, Marvel films and weird YouTube channels.",
+  },
+  {
+    name: "Yuriko Nishijima",
+    img: "/img/team/yuriko.png",
+    desc:
+      "Jumped into a Ethereum Space in early 2018. Now developing coolest L2 apps at CEL. World traveler since high school, professional foodie since a baby.",
+  },
+  {
+    name: "You",
+    img: "/img/team/profile.svg",
+    desc: "...and you might be a next one!",
+  },
+];
+
 function Team() {
   return (
     <div className={styles.root}>
@@ -15,31 +41,11 @@ function Team() {
       <hr />
 
       <div className="row">
-        <div className="col col--4">
-          <Card name="Syuhei Hiya" img="/img/team/syuhei.png">
-            Father of 2 lovely boys. Specializes in Programming Language Design.
-            Experienced startup CEO twice and he says CEL is the best team ever.
-          </Card>
-        </div>
-        <div className="col col--4">
-          <Card name="Takamichi Tsutsumi" img="/img/team/takamichi.png">
-            Used to be a JavaScript guy at a bunch of startups, while secretly
-            loving low stuff development. Loves watching NBA games, Marvel films
-            and weird YouTube channels.
-          </Card>
-        </div>
-        <div className="col col--4">
-          <Card name="Yuriko Nishijima" img="/img/team/yuriko.png">
-            Jumped into a Ethereum Space in early 2018. Now developing coolest
-            L2 apps at CEL. World traveler since high school, professional
-            foodie since a baby.
-          </Card>
-        </div>
-        <div className="col col--4">
-          <Card name="You" img="/img/team/profile.svg">
-            ...and you might be a next one!
-          </Card>
-        </div>
+        {cards.map((card, i) => (
+          <div className="col col--4">
+            <Card {...card} key={i} />
+          </div>
+        ))}
       </div>
       <div className="row">
         <div className={styles.card}>
