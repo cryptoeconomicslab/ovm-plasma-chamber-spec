0ba8d1ee22414821063b51f2dd2ab7e27dc5b3d6
Kiyohiko Heima <piyo326@gmail.com>
2020-04-24 12:53:06 +0900

    Add supporters & bottom CTA

---
+++ src/components/Supporters/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const supporters = [
+  {
+    img: "/img/supporters/ethereum_foundation.png",
+    title: "Ethereum Foundation",
+  },
+  {
+    img: "/img/supporters/tezos_foundation.png",
+    title: "Tezos Foundation",
+  },
+  {
+    img: "/img/supporters/web3_foundation.png",
+    title: "web3 foundation",
+  },
+];
+
+function Supporters() {
+  return (
+    <div className={styles.root}>
+      <h2 className={styles.headline}>Supporters</h2>
+      <div className={styles.supporters}>
+        {supporters.map(({ img, title }, i) => (
+          <div className={styles.supporter} key={i}>
+            <img
+              src={img}
+              title={title}
+              alt={title}
+              className={styles.supporter__img}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Supporters;
