b06c0391b305b0e808258f4ba05c0c20d2995f17
daiki.sekiguchi <daiki.skgc44@gmail.com>
2020-05-19 13:32:12 +0900

    update blog path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,7 +36,7 @@ module.exports = {
           position: "right"
         },
         {
-          href: "https://medium.com/cryptoeconomics-lab",
+          to: "blog",
           label: "Blog",
           position: "right"
         },
